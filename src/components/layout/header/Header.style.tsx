1406e8624197523633459a4ba37080b66fba0575
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-28 17:20:56 +0200

    Created the Header component with exaple data

---
+++ src/components/layout/header/Header.style.tsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+    margin: ${({ theme }) => theme.spaces.small} 0;
+    background-color: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.white};
+    border-radius: ${({ theme }) => theme.borderradiuses.big};
+
+    .logo {
+        font-weight: bold;
+        font-size: ${({ theme }) => theme.fontSizes.title};
+        padding: ${({ theme }) => theme.spaces.small + ' ' + theme.spaces.big};
+    }
+
+    .title {
+        font-size: ${({ theme }) => theme.fontSizes.small};
+    }
+
+    .value {
+        font-size: ${({ theme }) => theme.fontSizes.default};
+        font-weight: bold;
+    }
+
+    .header-data {
+        display: flex;
+        align-items: center;
+        gap: ${({ theme }) => theme.spaces.medium};
+    }
+
+    .emerald-account {
+        background-color: ${({ theme }) => theme.colors.gray1};
+        height: 100%;
+        display: flex;
+        align-items: end;
+        flex-direction: column;
+        justify-content: center;
+        padding: 0 ${({ theme }) => theme.spaces.big};
+        border-radius: 0 ${({ theme }) => theme.borderradiuses.big} ${({ theme }) => theme.borderradiuses.big} 0;
+    }
+`
+
+export default StyledHeader

eb92cf4042aaa9686749aeaba5efaf36bbd115f8
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-28 18:30:03 +0200

    Created the ListHeader component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import AppTheme from './theme/AppTheme'
 
 import Container from './components/layout/container/Container'
 import Header from './components/layout/header/Header'
+import ListHeader from './components/layout/listHeader/ListHeader'
 
 const App = () => {
   return <AppTheme>
     <Container>
       <Header />
+      <ListHeader />
     </Container>
   </AppTheme>
 }

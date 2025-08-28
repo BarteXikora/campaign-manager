c79cbe07df599d8273b5d83a80fe270af3512471
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-28 19:43:15 +0200

    Created the List component with mocked data

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import AppTheme from './theme/AppTheme'
 import Container from './components/layout/container/Container'
 import Header from './components/layout/header/Header'
 import ListHeader from './components/layout/listHeader/ListHeader'
+import List from './components/layout/list/List'
 
 const App = () => {
   return <AppTheme>
     <Container>
       <Header />
       <ListHeader />
+      <List />
     </Container>
   </AppTheme>
 }

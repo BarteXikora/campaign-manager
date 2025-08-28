9d00f4c6b44b51f7f0ba4e678e89f226d62b4e6f
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-28 21:13:36 +0200

    Created the ListScreen component with home page content

---
+++ src/components/screens/listScreen/ListScreen.tsx
@@ -0,0 +1,13 @@
+import ListHeader from '../../layout/listHeader/ListHeader'
+import List from '../../layout/list/List'
+
+const ListScreen = () => {
+    return (
+        <>
+            <ListHeader />
+            <List />
+        </>
+    )
+}
+
+export default ListScreen

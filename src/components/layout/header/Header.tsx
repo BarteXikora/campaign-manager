12ed652363e2424e5b1892da525aaa4261db853d
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-29 18:32:17 +0200

    Used data from store in the Headre component

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,23 +1,37 @@
 import StyledHeader from './Header.style'
 
+import { useSelector } from '../../../store/store'
+import { useState, useEffect } from 'react'
+
 const Header = () => {
+    const state = useSelector(state => state)
+
+    const [allCampaigntCnt, setAllCampaignsCnt] = useState<number>(0)
+    const [activeCampaigntCnt, setActiveCampaignsCnt] = useState<number>(0)
+
+    useEffect(() => {
+        setAllCampaignsCnt(state.campaigns.length)
+        setActiveCampaignsCnt(state.campaigns.filter(c => c.statusActive).length)
+
+    }, [state.campaigns])
+
     return <StyledHeader>
         <div className="logo">CampaignManager</div>
 
         <div className="header-data">
             <div className="data-box">
                 <span className='title'>All: </span>
-                <span className='value'>3</span>
+                <span className='value'>{allCampaigntCnt}</span>
             </div>
 
             <div className="data-box">
                 <span className='title'>Active: </span>
-                <span className='value'>2</span>
+                <span className='value'>{activeCampaigntCnt}</span>
             </div>
 
             <div className="emerald-account">
                 <span className='title'>Emerald Account: </span>
-                <span className='value'>1000 $</span>
+                <span className='value'>{state.emeraldAccount} $</span>
             </div>
         </div>
     </StyledHeader>

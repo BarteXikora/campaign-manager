197c1e95fff8aa4cefe5b140f3df834cce2d1003
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-29 12:51:16 +0200

    Created the CreateActions component with form submit button

--- src/components/screens/createScreen/CreateScreen.tsx
+++ src/components/screens/createScreen/CreateScreen.tsx
@@ -1,11 +1,13 @@
 import CreateFormHeader from '../../layout/createFormHeader/CreateFormHeader'
 import Form from '../../layout/form/Form'
+import CreateActions from '../../layout/createActions/CreateActions'
 
 const CreateScreen = () => {
     return (
         <>
             <CreateFormHeader />
             <Form />
+            <CreateActions />
         </>
     )
 }

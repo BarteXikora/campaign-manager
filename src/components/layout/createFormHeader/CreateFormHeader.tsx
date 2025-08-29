b5cfceab319cafd41880109330b872708ba76da9
Bartłomiej Sikora <sbartek50@gmail.com>
2025-08-29 17:29:06 +0200

    Removed coponents replaced with the TitleBar

--- src/components/layout/createFormHeader/CreateFormHeader.tsx
+++
@@ -1,9 +0,0 @@
-import StyledCreateFormHeader from './CreateFormHeader.style'
-
-const CreateFormHeader = () => {
-    return <StyledCreateFormHeader>
-        <h2>Create new campaign:</h2>
-    </StyledCreateFormHeader>
-}
-
-export default CreateFormHeader
